92e4ac0cc920d24bdb87c9ceaec38e4aa7883ef5
Sichang Li <seeking.scli@gmail.com>
2013-05-19 22:55:21 -0400

    added userlist

--- src/projectmate/backend/servlets/GetProjectInfo.java
+++ src/projectmate/backend/servlets/GetProjectInfo.java
@@ -15,6 +15,7 @@ import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 import projectmate.backend.datastore.Datastore;
 import projectmate.backend.models.Project;
+import projectmate.backend.models.User;
 
 public class GetProjectInfo extends HttpServlet {
 
@@ -53,9 +54,19 @@ public class GetProjectInfo extends HttpServlet {
 				r.put("deadline", p.getDeadline());
 				r.put("descr", p.getDescr());
 				r.put("owner", p.getOwner());
-				JSONArray members = new JSONArray();
 				
+				JSONArray members = new JSONArray();
+				ArrayList<User> userlist = p.getUserlist();
+				for (User user : userlist) {
+					JSONObject u = new JSONObject();
+					u.put("userid", user.getUserId());
+					u.put("lastName", user.getLastName());
+					u.put("firstName", user.getFirstName());
+					u.put("sex", user.getSex());
+					members.put(u);
+				}
 				r.put("members", members);
+				
 				arr.put(r);
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
